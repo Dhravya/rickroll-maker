d4bac59733ae6b357de8dde19cce3eb257ab71b0
Dhravya <dhravyashah@gmail.com>
2022-03-10 18:32:37 +0530

    added rickroller

--- src/main.py
+++ src/main.py
@@ -1,11 +1,25 @@
+from moviepy.editor import VideoFileClip, concatenate_videoclips
 
-# Imports here
 
+class Rickroller:
 
-# Code here
-def main():
-    print("Hello World")
+    @staticmethod
+    def clip_maker(clip_path: str, output_path="output.mp4"):
+        rickroll = VideoFileClip("rickroll.mp4")
+
+        clip2 = VideoFileClip(clip_path)
+        clip2 = clip2.subclip(0, clip2.duration - 15)
+
+        # Get the 15 seconds only
+        rickroll = rickroll.subclip(0.5, 15).resize(width=clip2.w)
+
+        # Concatenate the two clips
+        final_clip = concatenate_videoclips([clip2, rickroll], method="compose")
+
+        final_clip.write_videofile(output_path)
+
 
 if __name__ == "__main__":
-    main()
-    
+    Rickroller().clip_maker(
+        r"C:\Users\dhrav\Pictures\274309341_124665780124890_905868432853985379_n.mp4"
+    )
